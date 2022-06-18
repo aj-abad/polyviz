f34d4388dbbd178a8dbcb1855e564b60cf031866
AJ <ajabad.dev@gmail.com>
2022-06-18 16:33:44 +0800

    Added app title

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,10 @@
 
 </script>
 
-<main>
-	<h1>
-		Polyviz
-	</h1>
+  <div id="polyviz" class="py-5 text-center">
+		<h1>Polyviz</h1>
+		<h2 class="h5 text-muted fw-light">
+			Polyrhythm Visualizer
+		</h2>
+	</div>
 </main>
