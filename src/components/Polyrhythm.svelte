bcf86f21a1815ca6815948a006576da9703c8790
AJ <ajabad.dev@gmail.com>
2022-06-18 16:35:14 +0800

    Added default polyrhythm value

--- src/components/Polyrhythm.svelte
+++ src/components/Polyrhythm.svelte
@@ -1,6 +1,16 @@
 <script>
+  let polyrhythm = [2, 3];
 </script>
 
 <div class="d-flex justify-content-center">
+  {#each polyrhythm as poly, i}
+    <button class="btn btn-text" on:pointerdown={pointerDownHandler}>
+      {poly}
+    </button>
+
+    {#if i < polyrhythm.length - 1}
+      <button class="btn btn-text" disabled> : </button>
+    {/if}
+  {/each}
 </div>
 
